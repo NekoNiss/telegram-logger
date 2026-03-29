9a71c1ac9d029cf67f2fd2758e6924625ebb82ac
NekoNiss <103840040+NekoNiss@users.noreply.github.com>
2026-03-29 18:37:45 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,32 @@
 from fastapi import FastAPI, UploadFile, File
 import os
+import shutil
 
 app = FastAPI()
 
 SESSIONS_DIR = "sessions"
 
-# создаём папку
 os.makedirs(SESSIONS_DIR, exist_ok=True)
+
+print("🚀 SERVER START")
+
 
 @app.get("/")
 async def root():
-    return {"status": "OK"}
+    return {"status": "ok"}
+
 
 @app.get("/sessions")
 async def get_sessions():
-    try:
-        files = os.listdir(SESSIONS_DIR)
-        return {"sessions": files}
-    except Exception as e:
-        return {"error": str(e)}
+    files = os.listdir(SESSIONS_DIR)
+    return {"sessions": files}
+
 
 @app.post("/upload")
 async def upload_session(file: UploadFile = File(...)):
-    try:
-        path = os.path.join(SESSIONS_DIR, file.filename)
+    path = os.path.join(SESSIONS_DIR, file.filename)
 
-        with open(path, "wb") as f:
-            f.write(await file.read())
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
-        return {"status": "uploaded"}
-    except Exception as e:
-        return {"error": str(e)}
+    return {"status": "uploaded", "file": file.filename}
